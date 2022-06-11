e4890948ee9492b26afe2fde2a424566f7d4b885
dung.le <dunga1k58bh@gmail.com>
2022-06-11 14:18:22 +0700

    .

--- GK/Bonobo/cellphones.py
+++ GK/Bonobo/cellphones.py
@@ -4,44 +4,54 @@ import requests
 from bonobo.config import use_context_processor
 from bonobo.config import use
 import time
+from bs4 import BeautifulSoup
 
 def get_phone(page):
     return f'https://cellphones.com.vn/lapi/LoadMoreProductCate/index/?page={page}&id=3&order=view_count2&dir=desc&fearture=flashsale_samsung'
 
-def extract_phones():
-    yield {'name': 'name', 'storage': 'storage', 'display_size': 'display_size',
-     'price': 'price', 'final_price': 'price', 'stock_available': 'stock_available', 'url': 'url'  }
+def extractPhones():
     for i in range(1, 50):
         yield from requests.get(get_phone(i)).json()
 
-def transform_phones(*args):
-    """Placeholder, change, rename, remove... """
+def basicInfo(*args):
     if (args[0]['final_price']):
         price = args[0]['final_price']
     else :
         price = args[0]['price']
-    yield [
-        args[0]['name'],
-        args[0]['storage'],
-        args[0]['display_size'],
-        price,
-        args[0]['stock_available'],
-        args[0]['url']
-    ]
+    object_phone = {}
+    object_phone['name'] = args[0]['name']
+    object_phone['price'] = price
+    object_phone['url'] = args[0]['url']
 
-def load_phones(*args):
-    """Placeholder, change, rename, remove... """
-    with open("cellphones.csv" , "a") as f:
-        csv_out = csv.writer(f)
-        csv_out.writerow(list(args[0]))
+    url = args[0]['url']
+    response = requests.get(url).text
+    soup = BeautifulSoup(response)
+    div_contain = soup.find("div", {"class": "block-technical-info"})
+    table = div_contain.find("table", {"id": "tskt"})
+    table
+    rows = table.find_all("tr")
+    for row in rows:
+        data = row.find_all("th")
+        label = data[0].getText().replace(',', ' ').replace(';',' ')
+        value = data[1].getText().replace(',', ' ').replace(';',' ')
+        object_phone[label] = value
+
+    yield object_phone
+
+
+def loadPhones(*args):
+        keys = []
+        for key, value in args[0].items():
+            keys.append(str(key))
+
+        separate = '; '
+        line = separate.join(keys)
+        print(line)
+
 
 def get_graph(**options):
     graph = bonobo.Graph()
-    graph.add_chain(
-        extract_phones,
-        transform_phones,
-        load_phones,
-    )
+    graph >> extractPhones >> basicInfo >> loadPhones
     return graph
 
 
