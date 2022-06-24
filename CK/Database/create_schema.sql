26106e0d325d47920cfb9dcbf61d5aa7a7d3faf9
dung.le <dunga1k58bh@gmail.com>
2022-06-24 21:34:00 +0700

    schema_warehouse

---
+++ CK/Database/create_schema.sql
@@ -0,0 +1,18 @@
+create table if not exists phone(
+    id int primary key auto_increment,
+    name varchar(127),
+
+    data text,
+
+    color varchar(255),
+    display text,
+    ram varchar(255),
+    rom varchar(255),
+    chip text,
+    operation_system text,
+    camera text,
+    camera_selfle text,
+    battery int,
+    weight int
+
+);
