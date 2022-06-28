46812af5549b6e2c885f9304bfef0ba4c47b7669
ntd275 <ntd275@gmail.com>
2022-06-29 00:10:24 +0700

    update web

--- CK/Web/frontend/src/components/Product.js
+++ CK/Web/frontend/src/components/Product.js
@@ -2,18 +2,18 @@ import React from 'react'
 import "./product.scss"
 import {useNavigate} from "react-router-dom"
 
-function Product() {
+function Product(props) {
   const navigate = useNavigate();
   return (
-    <div className='item-container' onClick={() => navigate("/1")}>
-        <div className='name'>Iphone 13</div>
-        <div className='system'>Hệ điều hành: IOS</div>
-        <div className='chip'>Chip: Snapdragon 888</div>
-        <div className='ram'>RAM: 2GB</div>
-        <div className='rom'>ROM: 64GB</div>
-        <div className='monitor'>Màn hình: OLED</div>
-        <div className='pin'>Pin: 3000 mAh</div>
-        <div className='color'>Màu sắc: Đỏ</div>
+    <div className='item-container' onClick={() => navigate(`/${props.id}`)}>
+        <div className='name'>{props.name}</div>
+        <div className='system'><b>Hệ điều hành:</b> {props.operating_system}</div>
+        <div className='chip'><b>CPU:</b> {props.cpu}</div>
+        <div className='ram'><b>RAM:</b> {props.ram}GB</div>
+        <div className='rom'><b>ROM:</b> {props.rom}GB</div>
+        <div className='monitor'><b>Màn hình:</b> {props.display_tech}</div>
+        <div className='pin'><b>Pin:</b> {props.battery} mAh</div>
+        <div className='color'><b>Trọng lượng:</b> {props.weight} gram</div>
     </div>
   )
 }
