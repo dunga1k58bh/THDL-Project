3fc5192725fde5e2ca2259c4419a7c0b0dbae0d8
ntd275 <ntd275@gmail.com>
2022-06-29 10:54:16 +0700

    update web

--- CK/Web/backend/controllers/db.js
+++ CK/Web/backend/controllers/db.js
@@ -1,7 +1,7 @@
 const knex = require('knex')({
     client: 'mysql',
     connection: {
-        host : '20.212.104.107',
+        host : 'localhost',
         port : 3306,
         user : 'ntd275',
         password : 'nguyentheduc',
